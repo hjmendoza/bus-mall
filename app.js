cb36d8a0cd88a6bdadf20e7d5b58e83a96dd84e8
Haley Mendoza <mendozahaley@gmail.com>
2018-02-10 12:58:12 -0800

    Created product constructor function. Linked product imgs to html.

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+Product.allProducts = [];
+
+function Product(name, filepath) {
+  this.name = name;
+  this.filepath = filepath;
+  Product.allProducts.push(this);
+}
+
+new Product('bag', 'img/bag.jpg');
+new Product('banana', 'img/banana.jpg');
+new Product('bathroom', 'img/bathroom.jpg');
+new Product('boots', 'img/boots.jpg');
+new Product('breakfast', 'img/breakfast.jpg');
+new Product('bubblegum', 'img/bubblegum.jpg');
+new Product('chair', 'img/chair.jpg');
+new Product('cthulhu', 'img/cthulhu.jpg');
+new Product('dog-duck', 'img/dog-duck.jpg');
+new Product('dragon', 'img/dragon.jpg');
+new Product('pen', 'img/pen.jpg');
+new Product('pet-sweep', 'img/pet-sweep.jpg');
+new Product('tauntaun', 'img/tauntaun.jpg');
+new Product('unicorn', 'img/unicorn.jpg');
+new Product('usb', 'img/usb.jpg');
+new Product('water-can', 'img/water-can.jpg');
+new Product('wine-glass', 'img/wine-glass.jpg');
+
+// var clickCount = 0;
+
+
+
+// var imgEl = document.getElementById('product-pic');
