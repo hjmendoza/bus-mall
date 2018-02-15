25635bc822e14f3d757f57def2edf66d67647206
Haley Mendoza <mendozahaley@gmail.com>
2018-02-14 22:12:22 -0800

    All pics showing, no repeats. Results list working.

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 'use strict';
 
-var allProducts = [];
+Product.all = [];
+Product.container = document.getElementById('image_container');
+Product.justViewed = [];
+Product.pics = [document.getElementById('pic1'), document.getElementById('pic2'), document.getElementById('pic3')];
+Product.tally = document.getElementById('tally');
+Product.totalClicks = 0;
 
-function Product(name, filepath, showCount, clickCount) {
+
+function Product(name, filepath) {
   this.name = name;
   this.filepath = filepath;
-  this.showCount = showCount;
-  this.clickCount = clickCount;
-  allProducts.push(this);
+  this.votes = 0;
+  this.views = 0;
+  Product.all.push(this);
 }
-var previousSelection = {};
 
 new Product('bag', 'img/bag.jpg');
 new Product('banana', 'img/banana.jpg');
@@ -29,35 +34,64 @@ new Product('usb', 'img/usb.gif');
 new Product('water-can', 'img/water-can.jpg');
 new Product('wine-glass', 'img/wine-glass.jpg');
 
-
-var threePics = [];
-
-function displayRandomProduct() {
-
-  let currentSelection = {};
-  function randomIndexGen () {
-    let randInd = Math.floor(Math.random() * allProducts.length);
-    while (currentSelection[randInd] || previousSelection[randInd]) {
-      randInd = Math.floor(Math.random() * allProducts.length);
-    }
-    currentSelection[randInd] = 1;
-    console.log('CURRENT: ', currentSelection);
-    console.log('PREVIOUS: ', previousSelection);
-    return randInd;
-  }
-
-  function singleImg(path) {
-    let img = document.getElementById(path);
-    img.addEventListener('click', displayRandomProduct);
-    var randomIndex = randomIndexGen();
-    img.src = allProducts[randomIndex].filepath;
-    threePics.push(img);
-    return img;
-  }
-  singleImg('product-pic1');
-  singleImg('product-pic2');
-  singleImg('product-pic3');
-  previousSelection = currentSelection;
+function randomIndexGen(){
+  return Math.floor(Math.random() * Product.all.length);
 }
 
+function displayRandomProduct(){
+  var currentSelection = [];
+  currentSelection[0] = randomIndexGen();
+  while(Product.justViewed.indexOf(currentSelection[0]) !== -1){
+    console.error('Repeat. Re-run.');
+    currentSelection[0] = randomIndexGen();
+  }
+  currentSelection[1] = randomIndexGen();
+  while(currentSelection[0] === currentSelection[1] || Product.justViewed.indexOf(currentSelection[1]) !== -1){
+    console.error('Repeat. Re-run.');
+    currentSelection[1] = randomIndexGen();
+  }
+  currentSelection[2] = randomIndexGen();
+  while(currentSelection[0] === currentSelection[2]|| currentSelection[1] === currentSelection[2] || Product.justViewed.indexOf(currentSelection[2]) !== -1){
+    console.error('Repeat. Re-run.');
+    currentSelection[2] = randomIndexGen();
+  }
+
+  for(var i = 0; i < 3; i++){
+    Product.pics[i].src = Product.all[currentSelection[i]].filepath;
+    Product.pics[i].id = Product.all[currentSelection[i]].name;
+    Product.all[currentSelection[i]].views += 1;
+    Product.justViewed[i] =
+    currentSelection[i];
+  }
+}
+
+function handleClick(event){
+  console.log(Product.totalClicks, 'total clicks');
+  if(Product.totalClicks > 24){
+    Product.container.removeEventListener('click', handleClick);
+    showTally();
+  }
+  if(event.target.id === 'image-container'){
+    return alert('Please click on an image.');
+  }
+  Product.totalClicks += 1;
+  for(var i = 0; i < Product.all.length; i++){
+    if(event.target.id === Product.all[i].name){
+      Product.all[i].votes +=1;
+      console.log(event.target.id + ' has ' + Product.all[i].votes + ' votes in ' + Product.all[i].views + ' views.');
+    }
+  }
+  displayRandomProduct();
+}
+
+function showTally(){
+  for(var i = 0; i < Product.all.length; i++){
+    var liEl = document.createElement('li');
+    liEl.textContent = Product.all[i].name + ' has ' + Product.all[i].votes + ' votes in ' + Product.all[i].views + ' views.';
+    Product.tally.appendChild(liEl);
+  }
+}
+
+Product.container.addEventListener('click', handleClick);
 displayRandomProduct();
+
